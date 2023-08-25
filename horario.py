f6fd7b9063bbd479e350b325c99988170ca8419a
kauanburger <kauanburger007@gmail.com>
2023-08-25 17:39:04 -0300

    Até o meio dia
    Adicionando a função para ver quanto tempo falta para o meio-dia

--- horario.py
+++ horario.py
@@ -2,10 +2,13 @@ from datetime import datetime, timezone, timedelta
 
 fuso = timezone(timedelta(hours=-3))
 horario_horas = int(datetime.now().time().strftime('%H'))
+
 horario = datetime.now().astimezone(fuso)
+
 horario2 = horario.strftime('%H:%M')
 horario3 = int(horario.strftime('%H'))
 horario4 = int(horario.strftime('%M'))
+
 nome = input('Qual o seu nome? ')
 
 if (horario_horas >= 0 and horario_horas < 12 ):
@@ -18,16 +21,17 @@ else:
 meio_dia_hora = int(12)
 meio_dia_minutos = int(60)
 
-diferenca_hora = meio_dia_hora - horario3
+diferenca_hora = (meio_dia_hora - horario3) - 1
 diferenca_minuto = meio_dia_minutos - horario4
 
-if (diferenca_hora == 0 and diferenca_minuto == 0) :
-    print('Já são meio dia')
-elif(diferenca_hora > 0) :
-    print('Faltam {} horas e {} minutos até meio dia'.format(diferenca_hora, diferenca_minuto))
-elif(diferenca_hora == 0 and diferenca_minuto != 0):
-    print('Faltam {} minutos para meio dia'.format(diferenca_minuto))
-else:
-    print('Horário não correspondente')
+if (diferenca_hora == 0 and diferenca_minuto == 60) :
+    print('Já são meio-dia')
+elif(diferenca_hora > 1 and diferenca_minuto == 60) :
+    diferenca_hora = diferenca_hora + 1
+    print('Faltam {} horas até o meio-dia'.format(diferenca_hora))
+elif(diferenca_hora > 0 and diferenca_minuto < 60):
+    print('Faltam {} minutos para meio-dia'.format(diferenca_minuto))
+
 
 print('No horário de Brasília são: {}'.format(horario2))
+
