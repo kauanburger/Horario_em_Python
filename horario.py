710be66fc5efd76ce96153166c099c86773ad498
kauanburger <kauanburger007@gmail.com>
2023-08-26 00:30:27 -0300

    Noite_Update
    Adicionando código para calcular quanto tempo falta para chegar a noite

--- horario.py
+++ horario.py
@@ -3,11 +3,10 @@ from datetime import datetime, timezone, timedelta
 fuso = timezone(timedelta(hours=-3))
 horario_horas = int(datetime.now().time().strftime('%H'))
 
-horario = datetime.now().astimezone(fuso)
+horario_fuso = datetime.now().astimezone(fuso)
 
-horario2 = horario.strftime('%H:%M')
-horario3 = int(horario.strftime('%H'))
-horario4 = int(horario.strftime('%M'))
+horario_atual = horario_fuso.strftime('%H:%M')
+horario_minutos = int(horario_fuso.strftime('%M'))
 
 nome = input('Qual o seu nome? ')
 
@@ -19,19 +18,60 @@ else:
     print(f'Boa noite {nome}!!')
 
 meio_dia_hora = int(12)
-meio_dia_minutos = int(60)
+dia_minutos = int(60)
+tarde_hora = int(18)
 
-diferenca_hora = (meio_dia_hora - horario3) - 1
-diferenca_minuto = meio_dia_minutos - horario4
+diferenca_hora = meio_dia_hora - horario_horas
+diferenca_hora1 = tarde_hora - horario_horas
+diferenca_minuto = dia_minutos - horario_minutos
+
+#Quanto tempo falta até o meio-dia
 
 if (diferenca_hora == 0 and diferenca_minuto == 60) :
     print('Já são meio-dia')
 elif(diferenca_hora > 1 and diferenca_minuto == 60) :
-    diferenca_hora = diferenca_hora + 1
-    print('Faltam {} horas até o meio-dia'.format(diferenca_hora))
-elif(diferenca_hora > 0 and diferenca_minuto < 60):
-    print('Faltam {} minutos para meio-dia'.format(diferenca_minuto))
+    diferenca_hora = diferenca_hora
+    if (diferenca_hora1 == 1) :
+        print('Falta {} hora até o meio-dia'.format(diferenca_hora))
+    else :
+        print('Faltam {} horas até o meio-dia'.format(diferenca_hora))
+elif(diferenca_hora > 1 and diferenca_minuto < 60):
+    diferenca_hora = diferenca_hora - 1
+    if (diferenca_hora == 1) :
+        print('Falta {} hora e {} minutos até o meio-dia'.format(diferenca_hora, diferenca_minuto))
+    else :
+        print('Faltam {} horas e {} minutos até o meio-dia'.format(diferenca_hora, diferenca_minuto))
+elif(diferenca_hora == 1 and diferenca_minuto < 60):
+    if(diferenca_minuto == 1):
+        print('Falta {} minuto para o meio-dia'.format(diferenca_minuto))
+    else:
+        print ('Faltam {} minutos até o meio-dia'.format(diferenca_minuto))
+else:
+    print
 
+#Quanto tempo falata até a noite chegar
 
-print('No horário de Brasília são: {}'.format(horario2))
+if (horario_horas >= 12 and diferenca_hora1 == 0 and diferenca_minuto == 60) :
+    print('Agora são 18 horas, está começando a noite!!')
+elif(horario_horas >= 12 and diferenca_hora1 >= 1 and diferenca_minuto == 60):
+    diferenca_hora1 = diferenca_hora1
+    if (diferenca_hora1 == 1) :
+        print('Falta {} hora até a noite'.format(diferenca_hora1))
+    else :
+        print('Faltam {} horas até a noite'.format(diferenca_hora1))
+elif(horario_horas >= 12 and diferenca_hora1 > 1 and diferenca_minuto < 60) :
+    diferenca_hora1 = diferenca_hora1 - 1
+    if (diferenca_hora1 == 1) :
+        print('Falta {} hora e {} minutos até a noite'.format(diferenca_hora1, diferenca_minuto))
+    else :
+        print('Faltam {} horas e {} minutos até a noite'.format(diferenca_hora1, diferenca_minuto))
+elif (horario_horas >= 12 and diferenca_hora1 == 1 and diferenca_minuto < 60) :
+    if (diferenca_minuto == 1) :
+        print('Falta {} minuto até a noite'.format(diferenca_minuto))
+    else:
+        print('Faltam {} minutos até a noite'.format(diferenca_minuto))
+else:
+    print
+
+print('No horário de Brasília são: {}'.format(horario_atual))
 
